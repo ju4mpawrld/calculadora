2be228e1de45c7f1a9970fa33d570fcd9f69c356
Juan Pablo <juanpabloveracanales@gmail.com>
2023-08-29 19:26:45 -0400

    primer repositorio

---
+++ tarea1.py
@@ -0,0 +1,38 @@
+print("Bienvenido a la mejor calculadora")
+print("Para salir solo escribe salir")
+print("las operaciones son: suma, resta, multiplicacion y division")
+
+resultado = 0
+while True:
+    if resultado == 0:
+        resultado = int(input("ingrese numero: "))
+        if resultado == "salir":
+            break
+    op = input("ingresa operacion: ")
+    if op.lower() == "salir":
+        break
+    n2 = input("ingresa siguiente numero: ")
+    if n2.lower() == "salir":
+        break
+    n2 = int(n2)
+
+    if op.lower() == "suma":
+        resultado += n2
+    elif op.lower() == "resta":
+        resultado -= n2
+    elif op.lower() == "multiplicacion":
+        resultado *= n2
+    elif op.lower() == "division":
+        if n2 != 0:
+            resultado /= n2
+        else:
+            print("No se puede dividir entre cero.")
+            break
+    else:
+        print("operacion no valida")
+        break
+
+    print(f"el resultado es {resultado}")
+
+# nose que le pasa pero cualquier cosa esta en el curso (el del curso no me funciono xd)
+print("Gracias por utilizar la calculadora.")
